25b60e02c358e8147256d754e51bb13eea653a3c
nlprj <lawrence.nb@gmail.com>
2022-07-22 10:51:15 -0400

    remove ghn

--- thumbnails/XmlToCsv.py
+++ thumbnails/XmlToCsv.py
@@ -365,11 +365,6 @@ for f in files:
             repo = "youtube"
             mods = modifications(repo, edm_is_shown_by)
             urls = mods.repoWork()
-        elif "ghn" in edm_is_shown_by or "gahistoricnewspapers" in edm_is_shown_by:
-            repo = "ghn"
-
-            mods = modifications(repo, edm_is_shown_by)
-            urls = mods.repoWork()
         elif "#item" in edm_is_shown_by:
             repo = "dlg"
             mods = modifications(repo, edm_is_shown_by)
